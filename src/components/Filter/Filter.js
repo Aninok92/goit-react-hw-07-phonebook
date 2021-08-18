195affda05e3a505bce2b935514ef3686d9863b5
Nina Vasilieva <aninok92@gmail.com>
2021-08-18 13:16:00 +0300

    добавляет валидацию формы

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,5 +1,5 @@
 import { useSelector, useDispatch } from "react-redux";
-import { changeFilter} from "../../redux/contacts/contacts-actions";
+import { changeFilter } from "../../redux/contacts/contacts-actions";
 import { getFilter } from "../../redux/contacts/contacts-selectors";
 import s from "./Filter.module.scss";
 
@@ -11,11 +11,17 @@ const Filter = () => {
     <label className={s.label}>
       <span className={s.labelText}>Find contacts by name</span>
       <input
+        name="name"
+        placeholder="Leo"
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+        required
         className={s.input}
         type="text"
         value={value}
-       onChange={(e) => dispatch(changeFilter(e.target.value))}
-      ></input>
+        autoComplete="off"
+        onChange={(e) => dispatch(changeFilter(e.target.value))}
+      />
     </label>
   );
 };
